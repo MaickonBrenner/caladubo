6ddc33c738e8ad0cfaa12efc55b704199972f541
Maickon_Brenner <maickonbrenner@gmail.com>
2024-09-20 14:20:28 -0300

    Adicionando Menu Dropdown

---
+++ lib/MenuItems.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'homePage.dart';
+
+class MenuItem {
+  const MenuItem({
+    required this.text,
+    required this.icon,
+  });
+
+  final String text;
+  final IconData icon;
+}
+
+abstract class MenuItems {
+  static const List<MenuItem> firstItems = [home, share, settings];
+  static const List<MenuItem> secondItems = [logout];
+
+  static const home = MenuItem(text: 'Home', icon: Icons.home);
+  static const share = MenuItem(text: 'Share', icon: Icons.share);
+  static const settings = MenuItem(text: 'Settings', icon: Icons.settings);
+  static const logout = MenuItem(text: 'Log Out', icon: Icons.logout);
+
+
+  static Widget buildItem(MenuItem item) {
+    return Row(
+      children: [
+        Icon(item.icon, color: Colors.white, size: 22),
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: Text(
+            item.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  static void onChanged(BuildContext context, MenuItem item) {
+    switch (item) {
+      case MenuItems.home:
+       /*ChatHome chatHome = new ChatHome();
+        Navigator.push(
+          context as BuildContext,
+          MaterialPageRoute(builder: (context) => chatHome),
+        );*/
+        break;
+      case MenuItems.settings: //Configurações
+        /*ChatPage chatPage = new ChatPage();
+        Navigator.push(
+          context as BuildContext,
+          MaterialPageRoute(builder: (context) => chatPage),
+          );
+        break;*/
+      case MenuItems.share:
+        /*Calendario calendario = new Calendario();
+        Navigator.push(
+          context as BuildContext,
+          MaterialPageRoute(builder: (context) => calendario),
+        );*/
+        break;
+      case MenuItems.logout:
+      /*telaLogin telalogin = new telaLogin();
+        Navigator.push(
+        context as BuildContext,
+        MaterialPageRoute(builder: (context) => telalogin),
+      );*/
+        break;
+    }
+  }
+}
+//https://pub.dev/packages/dropdown_button2
--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,5 +1,6 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
-
+import 'MenuItems.dart';
 import 'colors.dart';
 
 class homePage extends StatelessWidget {
@@ -31,11 +32,58 @@ class homePageState extends State<homePageNew> {
     return MaterialApp(
       theme: ThemeData(
         primaryColor: Color.fromRGBO(126, 175, 49, 1),
-        scaffoldBackgroundColor: Color.fromRGBO(251, 236, 217, 1)
+        scaffoldBackgroundColor: Color.fromRGBO(251, 236, 217, 1),
       ),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("Caladubo"),
+          actions: [
+            DropdownButtonHideUnderline(
+              child: DropdownButton2(
+                customButton: const Icon(
+                  Icons.list,
+                  size: 46,
+                  color: Colors.white,
+                ),
+                items: [
+                  ...MenuItems.firstItems.map(
+                    (item) => DropdownMenuItem<MenuItem>(
+                      value: item,
+                      child: MenuItems.buildItem(item)
+                    ),
+                  ),
+                  const DropdownMenuItem<Divider>(
+                      enabled: false, child: Divider()),
+                  ...MenuItems.secondItems.map(
+                    (item) => DropdownMenuItem<MenuItem>(
+                      value: item,
+                      child: MenuItems.buildItem(item),
+                    ),
+                  ),
+                ],
+                onChanged: (value) {
+                  MenuItems.onChanged(context, value! as MenuItem);
+                },
+                dropdownStyleData: DropdownStyleData(
+                  width: 160,
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: Color.fromRGBO(126, 175, 49, 1)
+                  ),
+                  offset: const Offset(0, 8),
+                ),
+                menuItemStyleData: MenuItemStyleData(
+                  customHeights: [
+                    ...List<double>.filled(MenuItems.firstItems.length, 48),
+                    8,
+                    ...List<double>.filled(MenuItems.secondItems.length, 48),
+                  ],
+                  padding: const EdgeInsets.only(left: 16, right: 16),
+                ),
+              ))
+          ],
         ),
         body: Center(
           child: Padding(
