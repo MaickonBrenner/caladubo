c01923acaf2c7912404c91553c768c38a01eed95
Maickon_Brenner <maickonbrenner@gmail.com>
2025-02-04 15:00:14 -0300

    Add Culturas

---
+++ lib/views/culturas.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'homePage.dart';
+
+class culturas extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: culturasNew(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class culturasNew extends StatefulWidget {
+  const culturasNew({Key? key}) : super(key: key);
+  @override
+  culturasState createState() {
+    return culturasState();
+  }
+}
+
+class culturasState extends State<culturasNew> {
+
+  void changePage() {
+    setState(() {
+      Navigator.push(
+        context,
+         MaterialPageRoute(builder: (context) => homePage()));
+    });
+  }
+
+  @override  
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(126, 175, 49, 1),
+        scaffoldBackgroundColor: Color.fromRGBO(251, 236, 217, 1),
+        colorScheme: ColorScheme.light(
+           primary: Color.fromARGB(255, 78, 124, 3),
+        )
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: changePage,
+          ),
+          title: const Text(
+            'Culturas',
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              color: Colors.white,
+              fontSize: 22,
+            ),
+          ),
+          backgroundColor: Color.fromRGBO(126, 175, 49, 1),
+        ),
+        body: SingleChildScrollView(
+          
+        ),
+      )
+    );
+  }
+}
